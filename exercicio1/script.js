bc07fdeed386facb12b383fdbf4ede13feabb85a
Lucas-Louback <lucasmlouback99@gmail.com>
2023-03-15 15:53:59 -0300

    exercícios prontos

---
+++ exercicio1/script.js
@@ -0,0 +1,28 @@
+console.log("Exercicio 1 a-e")
+const nome = prompt("Qual o seu nome?" )
+typeof nome
+const idade = Number( prompt("Qual a sua idade?" ) )
+typeof idade
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+//houve erro no console pois as constantes não foram declaradas
+// ao usar o comando porompt o erro foi corrigido
+console.log("Olá, seu nome é", nome, "e a sua idade é de", idade, "anos.")
+
+console.log("Parte 2 -----")
+const time = prompt("Qual o seu time?")
+typeof time
+const numero = Number( prompt("Qual o numero da sua camisa?") )
+typeof numero
+const original = confirm("Sua camisa é original?")
+typeof original
+
+console.log("O time da sua camisa é: ",time)
+console.log("O número da sua camisa é: ", numero)
+console.log("A sua camisa é original? ", original)
+console.log(typeof time)
+console.log(typeof numero)
+console.log(typeof original)
+
---
+++ exercicio2/script.js
@@ -0,0 +1,16 @@
+console.log("Exercício 2")
+let a = 10
+let b = 25
+let c = a
+
+console.log("a=",a)
+console.log("b=",b)
+
+
+console.log("------")
+
+a = b
+b = c
+
+console.log("a=",a)
+console.log("b=",b)
